a384b084ca13e25b7a6eeb262639c9c030b1f8a7
Cameron Way <cameron.way@designitcorp.ca>
2021-12-12 19:41:28 -0800

    Added header/footer

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import Nav from './nav'
+import React from 'react';
+import Nav from './nav';
 import { Link } from 'gatsby';
 
 const Header = () => {
-    return (
-        <div className='top-0 left-0 right-0 sticky w-full flex justify-between p-6 items-center rounded-xl bg-white opacity-95'>
-            <Link to='/' className="items-center text-2xl">Cameron Way</Link>
-            <Nav />
-        </div>
-    )
-}
+  return (
+    <div className="object-top w-full flex justify-between p-6 py-4 items-center rounded-xl bg-white opacity-95 z-50">
+      <Link to="/" className="items-center text-2xl">
+        Cameron Way
+      </Link>
+      <Nav />
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,16 +1,54 @@
-import React from 'react'
-import { Link } from 'gatsby';
-
+import React from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+// import resume from 'static/Resume.pdf';
 const Nav = () => {
-    return (
-        <ul className="flex flex-row">
-            <li><Link to="/about" href="/about" className="p-4 hover:text-gray-500 font-bold" >About</Link></li>
-            <li><a href="src/Resources/Resume.pdf" className="p-4 hover:text-gray-500 font-bold" download="Cameron Way's Resume">Resume</a></li>
-            <li><a href="https://github.com/cway14" className="p-4 hover:text-gray-500 font-bold">Github</a></li>
-            <li><a href="https://www.linkedin.com/in/camway/" className="p-4 hover:text-gray-500 font-bold" >Linkedin</a></li>
-            <li><Link to="/about" className="bg-black text-white px-4 hover:text-gray-300 font-bold py-3 rounded-lg items-center ml-4 mr-2"> View Work</Link></li>
-        </ul>
-    )
-}
 
-export default Nav
+    const data = useStaticQuery(graphql`
+    {
+      allFile(filter: { name: { eq: "Resume" } }) {
+        edges {
+          node {
+            publicURL
+            name
+          }
+        }
+      }
+    }
+  `)
+    const resumeURL = data.allFile?.edges[0] ? data.allFile.edges[0]?.node.publicURL : '';
+  return (
+    <ul className="flex flex-row">
+      <li>
+        <Link to="/about" href="/about" className="p-4 hover:text-gray-500 font-bold">
+          About
+        </Link>
+      </li>
+      <li>
+        <a href={resumeURL} className="p-4 hover:text-gray-500 font-bold" download="Cameron Way's Resume">
+          Resume
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/cway14" className="p-4 hover:text-gray-500 font-bold">
+          Github
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/camway/" className="p-4 hover:text-gray-500 font-bold">
+          Linkedin
+        </a>
+      </li>
+      <li>
+        <Link
+          to="/projects"
+          className="bg-black text-white px-4 hover:text-gray-300 font-bold py-3 rounded-lg items-center ml-4 mr-2"
+        >
+          {' '}
+          View Work
+        </Link>
+      </li>
+    </ul>
+  );
+};
+
+export default Nav;
