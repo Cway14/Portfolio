ca8ddedcf1fce25583cdda34a20e3a723149d1a4
Cameron Way <cameron.way@designitcorp.ca>
2022-01-27 11:54:04 -0800

    Added typescript start script

---
+++ FormApi/routes/routes.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var EmailService_1 = require("../Services/EmailService");
+var express = require('express');
+var emailService = new EmailService_1.emailService();
+var app = express();
+app.use(express.json());
+app.post('/', function (req, res) {
+    try {
+        emailService.sendMail(req.body);
+        res.send('Message sent!');
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+});
+app.listen(3001, function () { return console.log('Form API is running on port 3001.'); });
