c64c8c243c5c1a40fd749d477beca010420cd576
Cam Way <cameron.way@designitcorp.ca>
2022-02-09 21:03:17 -0800

    fixing graphql error

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,6 +2,7 @@ import { useStaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { graphql } from 'gatsby';
 
 function SEO({ description, lang, meta, keywords, title }) {
   const { site } = useStaticQuery(graphql`
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,52 +4,42 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 function ContactPage() {
+  const sendContactTest = () => {
+    const body = {
+      firstName: 'Cameron',
+      lastName: 'Way',
+      email: 'cway@sfu.ca',
+      phone: '123-456-789',
+      message: 'test',
+    };
+
+    fetch('https://api.camway.ca', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+
+  const sendGetTest = async () => {
+    await fetch('https://api.camway.ca')
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <Layout>
-      <SEO keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]} title="Contact" />
-      <section>
-        <form>
-          <h2 className="mb-8 leading-loose">
-            Here is an example of a form built using the official Tailwind CSS Forms plugin.{` `}
-            <a
-              className="font-bold text-gray-700 no-underline"
-              href="https://github.com/tailwindlabs/tailwindcss-forms"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read the docs
-            </a>
-            .
-          </h2>
-
-          <label className="block mb-2 text-xs font-bold uppercase" htmlFor="first-name">
-            First Name
-          </label>
-
-          <input className="w-full mb-6 rounded-md border-gray-800" id="first-name" placeholder="Bill" type="text" />
-
-          <label className="block mb-2 text-xs font-bold uppercase" htmlFor="last-name">
-            Last Name
-          </label>
-
-          <input className="w-full mb-6 rounded-md border-gray-800" id="last-name" placeholder="Murray" type="text" />
-
-          <label className="block mb-2 text-xs font-bold uppercase" htmlFor="message">
-            Message
-          </label>
-
-          <textarea
-            className="w-full mb-6 rounded-md border-gray-800"
-            id="message"
-            placeholder="Say something..."
-            rows="8"
-          />
-
-          <button className="px-4 py-2 text-sm font-bold text-white bg-gray-700 border-b-4 border-gray-800 rounded hover:border-gray-700 hover:bg-gray-600">
-            Submit
-          </button>
-        </form>
-      </section>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <button type="button" onClick={() => sendContactTest()}>
+          Test Post
+        </button>
+        <button type="button" onClick={() => sendGetTest()}>
+          Test Get
+        </button>
+      </div>
     </Layout>
   );
 }
